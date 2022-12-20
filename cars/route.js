297280ccc7253a363575f9101d4e9f0ddbe463ec
HukakHePak <hukakhepak@gmail.com>
2022-12-20 23:55:27 +0400

    try useful funcs

--- cars/route.js
+++ cars/route.js
@@ -2,12 +2,34 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('./mysql');
 
+
+
 router.get('/users', function (req, res) {
-    mysql.query('select * from carshow.user', (err, result) => {
+
+    // mysql.call('called procedure!')
+    // mysql.exec('called procedure!')
+    // let out;
+
+    // mysql.query('call show_user_w(@out)', [out], (err, result) => {
+    //     if(err) throw err;
+
+    //     res.send({ result, out });
+    // });
+    mysql.query('select get_users_count()', (err, result) => {
         if(err) throw err;
 
         res.send({ result });
     });
+    // mysql.query('call show_all_users()', (err, result) => {
+    //     if(err) throw err;
+
+    //     res.send({ result });
+    // });
+    // mysql.query('select * from user', (err, result) => {
+    //     if(err) throw err;
+
+    //     res.send({ result });
+    // });
 });
 
 module.exports = router;
--- server/mysql.js
+++ server/mysql.js
@@ -1,12 +1,36 @@
 const mysql = require('mysql'); 
 
+// mysql.proto
+// mysql.__proto__.call = function(procedure, ...params) {
+//     console.log(procedure);
+// }
+
+// Object.setPrototypeOf(mysql, { call(procedure, ...params) {
+//     console.log(procedure);
+// }})
+
+
 const pool = {
     db(database) {
-        return mysql.createPool({
+        const pool = mysql.createPool({
             user: process.env.MYSQL_USER,
             password: process.env.MYSQL_PASSWORD,
             database,
         });
+
+        pool.call = function(name, params, callback) {
+            pool.query(`call ${name}(${params.join(',')})`, function (err, result) {
+                callback(err, result[0]);
+            });
+        }
+
+        pool.exec = function(name, params, callback) {
+            pool.query(`select ${name}(${params.join(',')})`, function (err, result) {
+                callback(err, result[0].values[0]);
+            });
+        }
+
+        return pool;
     }
 };
 
