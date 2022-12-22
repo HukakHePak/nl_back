080e2ebcc12408d7412e0bb4eff1fce65f888743
HukakHePak <valsidalvonilb@outlook.com>
2022-12-22 18:51:59 +0400

    feat: eslint

--- server/mail.js
+++ server/mail.js
@@ -1,39 +1,44 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 const options = {
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-        user: process.env.MAIL_LOGIN,
-        pass: process.env.MAIL_KEY,
-    },
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.MAIL_LOGIN,
+    pass: process.env.MAIL_KEY,
+  },
 };
 
 const transporter = nodemailer.createTransport(options);
 
-var send = (to, sub) => console.error('not send before initial', to, sub);
+let send = (to, sub) => console.error('not send before initial', to, sub);
 
 const mailer = (...args) => send(...args);
 
-transporter.verify((err, success) => {
-    if (err) {
-        console.error('Mail init connection error.', err.response);
-    } else {
-        console.log('Ready to send mail!');
+transporter.verify((err) => {
+  if (err) {
+    console.error('Mail init connection error.', err.response);
+  } else {
+    console.log('Ready to send mail!');
 
-        send = (to, subject, content) => {
-            transporter.sendMail({
-                from: process.env.MAIL_LOGIN, to, subject,
-                html: content
-            }, function (err, reply) {
-                if (err) {
-                    console.error(err);
-                } else {
-                    console.log('mail sent', to, subject)
-                }
-            });
-        }
-    }
+    send = (to, subject, content) => {
+      transporter.sendMail(
+        {
+          from: process.env.MAIL_LOGIN,
+          to,
+          subject,
+          html: content,
+        },
+        (e) => {
+          if (e) {
+            console.error(e);
+          } else {
+            console.log('mail sent', to, subject);
+          }
+        },
+      );
+    };
+  }
 });
 
 module.exports = mailer;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
-require("dotenv").config({ path: "config.env" });
+require('dotenv').config({ path: 'config.env' });
 
-const mongo = require('./mongo');
 const mongoose = require('mongoose');
+const mongo = require('./mongo');
 
 require('express-ws')(app);
 const masagerSocket = require('../masager/socket');
@@ -15,40 +16,40 @@ const masager = require('../masager/route');
 const cars = require('../cars/route');
 const carsDb = require('../cars/mysql');
 
-app.use(cors({ origin: [/\.github\.io$/, /\:5000$/, /\:8000/, /\:1234/] }));
+app.use(cors({ origin: [/\.github\.io$/, /:5000$/, /:8000/, /:1234/] }));
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    mongo.connect(function (err, db) {
-        if (!db || err) {
-            throw new Error('MongoDB init connection error.');
-        }
-        console.log("Successfully connected to MongoDB.");
-    });
+  mongo.connect((err, db) => {
+    if (!db || err) {
+      throw new Error('MongoDB init connection error.');
+    }
+    console.log('Successfully connected to MongoDB.');
+  });
 
-    mongoose.connect(process.env.MONGOOSE)
+  mongoose
+    .connect(process.env.MONGOOSE)
     .then(() => console.log('Succesfully connected: Mongoose.'))
-    .catch((err) => {
-        throw new Error('Mongoose init connection error.')
+    .catch(() => {
+      throw new Error('Mongoose init connection error.');
     });
 
-    carsDb.getConnection(function(err, connection) {
-        if (err) {
-            throw new Error('MySQL init connection error.');
-        }
-        console.log("Successfully connected to db carshow.");
-    });
-    
-    app.use(express.static('/home/Note-Lawn/build/'));
+  carsDb.getConnection((err) => {
+    if (err) {
+      throw new Error('DB carshow init connection error.');
+    }
+    console.log('Successfully connected to db carshow.');
+  });
 
-    app.use('/masager/', (req, res) => res.redirect('https://hukakhepak.github.io/masager/'));
-    app.use('/api/masager/', masager);
-    app.use('/websocket/masager/', masagerSocket);
+  app.use(express.static('/home/Note-Lawn/build/'));
 
-    app.use('/api/cars/', cars);
+  app.use('/masager/', (req, res) => res.redirect('https://hukakhepak.github.io/masager/'));
+  app.use('/api/masager/', masager);
+  app.use('/websocket/masager/', masagerSocket);
 
-    console.log(`Server is running on port: ${port}`);
+  app.use('/api/cars/', cars);
+
+  console.log(`Server is running on port: ${port}`);
 });
-
